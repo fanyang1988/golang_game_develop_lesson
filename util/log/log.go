47843651d640657f3bd1055c8200aa68fd1be688
fanyang <fanyang.coder@gmail.com>
2016-07-17 18:08:28 +0800

    add util/log

---
+++ util/log/log.go
@@ -0,0 +1,43 @@
+package log
+
+import (
+	"fmt"
+	"github.com/cihub/seelog"
+	"runtime/debug"
+)
+
+func Init(path string) error {
+	logger, err := seelog.LoggerFromConfigAsFile(path)
+	if err != nil {
+		return err
+	}
+	seelog.ReplaceLogger(logger)
+	return nil
+}
+
+func Flush() {
+	seelog.Flush()
+}
+
+func Trace(format string, params ...interface{}) {
+	seelog.Tracef(format, params...)
+}
+
+func Debug(format string, params ...interface{}) {
+	seelog.Debugf(format, params...)
+}
+
+func Info(format string, params ...interface{}) {
+	seelog.Infof(format, params...)
+}
+
+func Error(format string, params ...interface{}) {
+	seelog.Errorf(format, params...)
+}
+
+func Critical(format string, params ...interface{}) {
+	seelog.Critical(
+		fmt.Sprintf(format, params...),
+		",",
+		debug.Stack())
+}
